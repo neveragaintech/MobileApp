05794b02063c8eb4cffeec61efad73b180b150d1
Prathyusha Pillari <>
2019-04-18 23:11:17 -0700

    edits/ fixes

--- MobileApp/screens/AboutUs.js
+++ MobileApp/screens/AboutUs.js
@@ -40,10 +40,17 @@ const nicole = Platform.select({
                                });
 
 const kundana = Platform.select({
-                               ios: 'Kundana Pillari\n' + 'Software Engineer',
+                               ios: 'Kundana Pillari\n' + 'Software Engineer                 ',
                                android:
                                'Kundana Pillari\n' + 'Software Engineer',
                                });
+
+const aboutTheApp = Platform.select({
+                                ios: 'This app, built by Neveragaintech, aims to help survivors and victims of gun violence treat the emotional trauma/PTSD effects of these horrific incidents. This app aims to create acceptance and reduce the stigma of PTSD with its forum and journal pages, where survivors can share their experiences and feelings with regards to hyper vigilance, survivors guilt, and fear among other things. The skills page focuses on coping strategies for survivors such as grounding and art therapy, and provides them in an accessible mobile format for immediate response in situations where certain memories are triggered.\n' + '\nWe are developing a page where survivors are connected to the closest trauma recovery psychologists in their area. Our mission is to dispel the stigma around PTSD/mental health effects of gun violence by providing building support communities and having resources at fingertips.',
+                                android:
+                                'This app, built by Neveragaintech, aims to help survivors and victims of gun violence treat the emotional trauma/PTSD effects of these horrific incidents. This app aims to create acceptance and reduce the stigma of PTSD with its forum and journal pages, where survivors can share their experiences and feelings with regards to hyper vigilance, survivors guilt, and fear among other things. The skills page focuses on coping strategies for survivors such as grounding and art therapy, and provides them in an accessible mobile format for immediate response in situations where certain memories are triggered.\n' + 'We are developing a page where survivors are connected to the closest trauma recovery psychologists in their area. Our mission is to dispel the stigma around PTSD/mental health effects of gun violence by providing building support communities and having resources at fingertips.',
+                                });
+
 
 export default class AboutUs extends React.Component{
     render() {
@@ -52,7 +59,7 @@ export default class AboutUs extends React.Component{
                 <View style={styles.container}>
                 
                 <Text style={styles.welcome}>Never Again Tech</Text>
-                <Text style={styles.about}>This app, built by Neveragaintech, aims to help survivors and victims of gun violence treat the emotional trauma/PTSD effects of these horrific incidents. This app aims to create acceptance and reduce the stigma of PTSD with its forum and journal pages, where survivors can share their experiences and feelings with regards to hyper vigilance, survivors guilt, and fear among other things. The skills page focuses on coping strategies for survivors such as grounding and art therapy, and provides them in an accessible mobile format for immediate response in situations where certain memories are triggered. We are developing a page where survivors are connected to the closest trauma recovery psychologists in their area. Our mission is to dispel the stigma around PTSD/mental health effects of gun violence by providing building support communities and having resources at fingertips.</Text>
+                <Text style={styles.about}>{aboutTheApp}</Text>
                 
                 <Text style={styles.welcome}>Mobile Development Team</Text>
                 {/* <Text style={styles.about}>#NeverAgainTech is a project that uses technology to predict and prevent mass shootings. We wanted to take what we knew best, technology and Artificial Intelligence, and work together with teens all across the nation to advocate for what we believe.</Text> */}
@@ -119,7 +126,7 @@ const styles = StyleSheet.create({
                                  borderRadius: 50/2,
                                  },
                                  about: {
-                                 margin: 15,
+                                 margin: 10,
                                  fontStyle: 'italic',
                                  textAlign: 'center',
                                  color: '#333333',
